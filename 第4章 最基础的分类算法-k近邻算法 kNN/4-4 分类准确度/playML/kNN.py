491aad789168d8be61d6d53ebbd269987f4bffa1
dcscodelife <dingchao1007@163.com>
2018-02-02 16:50:19 +0800

    4-4 分类准确度 学习完成

--- 第4章 最基础的分类算法-k近邻算法 kNN/4-4 分类准确度/playML/kNN.py
+++ 第4章 最基础的分类算法-k近邻算法 kNN/4-4 分类准确度/playML/kNN.py
@@ -1,10 +1,13 @@
 # coding: utf-8
+
 import numpy as np
 from math import sqrt # math包里面只用了sqrt这一个函数
 from collections import Counter
+from .metrics import accuracy_score # 为什么要加个小数点呢？
 
-class KNNClassifier:
 
+class KNNClassifier: 
+    # 为什么都要加入self, 类的成员函数才需要加入吧
     def __init__(self, k): # 凡是有参数的地方都需要进行断言
         """初始化kNN分类器模型"""
         assert k >= 1
@@ -34,8 +37,16 @@ class KNNClassifier:
         assert X_predict.shape[1] == self._X_train.shape[1], \
             "the feature number of X_predict must equal to X_train"
         # 调用内部函数都需要加self.
-        Y_predict = [self._predict(x_predict) for x_predict in X_predict]
-        return Y_predict
+        y_predict = [self._predict(x_predict) for x_predict in X_predict]
+        # return y_predict
+        return np.array(y_predict) # 用小写字母吧
+
+    def score(self, X_test, y_test):
+        # 断言，如果参数都传入了其他函数，没有之间使用参数，则不需要使用断言，因为出问题也是其他函数的事情
+        assert X_test.shape[0] == y_test.shape[0], \
+            "the size of X_test must be equal to the size of y_test"
+        y_predict = self.predict(X_test)
+        return accuracy_score(y_test, y_predict)
 
     # 最难的部分之前kNN函数已经写过了
     def _predict(self, x_predict):
@@ -56,5 +67,5 @@ class KNNClassifier:
         # 返回的应该是输入样本的预测值
         return votes.most_common(1)[0][0]
 
-        def __repr__(self):
-            return "KNN(k=%d)" %self.k # return self
+    def __repr__(self):
+        return "KNN(k=%d)" % self.k # return self
